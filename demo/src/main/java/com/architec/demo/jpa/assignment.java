c29343fef52472fcf990242da711b760517fa753
Yaramis Naseyb (s103338) <naseyb.yaramis@student.ap.be>
2019-04-24 15:59:33 +0200

    Added css and made java file for assignment

---
+++ demo/src/main/java/com/architec/demo/jpa/assignment.java
@@ -0,0 +1,92 @@
+package com.architec.demo.jpa;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+public class assignment {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String summary, lector, responsible, company, address, city, postalCode;
+
+    @Column
+    private Date startDate, endDate;
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLector() {
+        return lector;
+    }
+
+    public void setLector(String lector) {
+        this.lector = lector;
+    }
+
+    public String getResponsible() {
+        return responsible;
+    }
+
+    public void setResponsible(String responsible) {
+        this.responsible = responsible;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
