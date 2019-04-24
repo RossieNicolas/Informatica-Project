104d41a4498eac8163260a5234a41bd59f9e7cf6
VeraaK <karenveraa@live.be>
2019-04-24 11:58:41 +0200

    html doorgeven

--- demo/src/main/java/com/architec/demo/Repositories/userRepository.java
+++
@@ -1,8 +0,0 @@
-package com.architec.demo.Repositories;
-
-import org.springframework.data.repository.CrudRepository;
-import com.architec.demo.models.User;
-
-public interface userRepository extends CrudRepository<User, Integer> {
-    User findByName(String name);
-}
--- demo/src/main/java/com/architec/demo/controller/HelloController.java
+++ demo/src/main/java/com/architec/demo/controller/HelloController.java
@@ -1,20 +1,25 @@
 package com.architec.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HelloController {
+
+    private JDBCcontroller connectie;
 
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
     }
 
-    @GetMapping("/assignment")
-    public String getAllAssingments() {
-
+    @GetMapping("/allassignments")
+    public String getAllAssingments(@RequestParam(name = "input", required = false) String input, Model model) {
+        input = connectie.findAll();
+        model.addAttribute("name", input);
         return "listAllAssignments";
     }
 
