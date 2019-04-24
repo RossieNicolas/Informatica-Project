2df04bf9939bac50d69a794db3a99db3d3fb8663
Yaramis Naseyb (s103338) <naseyb.yaramis@student.ap.be>
2019-04-24 16:18:56 +0200

    test page (/assignment) for Create Assignment Form

--- demo/src/main/java/com/architec/demo/HelloController.java
+++ demo/src/main/java/com/architec/demo/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
         return "Greetings from SpringBoot";
     }
 
-    @RequestMapping("/index")
+    @RequestMapping("/assignment")
     public String test() {
         return "CreateAssignmentForm";
     }
