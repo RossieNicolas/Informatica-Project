527a6c2852cbf0e1d0c6ba63d9c5e745c80d5e90
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-04-25 15:53:34 +0200

    added tag form to demo app

---
+++ demo/src/main/java/com/architec/demo/jpa/TagRepo.java
@@ -0,0 +1,8 @@
+package com.architec.demo.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TagRepo extends JpaRepository<Tag, Integer> {
+}
