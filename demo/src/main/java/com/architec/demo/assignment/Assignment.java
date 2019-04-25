2f9140b57c353c2b7a8679a88ba241d2eec2e8ac
wa1id <wa1id@users.noreply.github.com>
2019-04-25 12:06:58 +0200

    connect to remote db and get dummy data

--- demo/src/main/java/com/architec/demo/assignment/Assignment.java
+++ demo/src/main/java/com/architec/demo/assignment/Assignment.java
@@ -1,64 +1,114 @@
 package com.architec.demo.assignment;
 
+import javax.persistence.*;
+
+//@Entity
 public class Assignment {
-    private int ficheID;
-    private String titel;
-    private String soort;
-    private String omschrijving;
-    private int aantalUren;
-    private int aantalStudenten;
-    private String beginDatum;
-    private String eindDatum;
-    private String opdrachtgever;
+//    @Id
+//    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Integer assignmentId;
+
+    private String title;
+    private String type;
+    private String task;
+    private int amountHours;
+    private int amountStudents;
+    private String startDate;
+    private String endDate;
+    private boolean archived;
+    private boolean validated;
+    private String extraInfo;
+    private String assignerUserId;
 
     public Assignment() {
     }
 
-    public Assignment(int ficheID, String titel, String soort, String omschrijving, int aantalUren, int aantalStudenten, String beginDatum, String eindDatum, String opdrachtgever) {
-        this.ficheID = ficheID;
-        this.titel = titel;
-        this.soort = soort;
-        this.omschrijving = omschrijving;
-        this.aantalUren = aantalUren;
-        this.aantalStudenten = aantalStudenten;
-        this.beginDatum = beginDatum;
-        this.eindDatum = eindDatum;
-        this.opdrachtgever = opdrachtgever;
+    public Assignment(String title, String type, String task, int amountHours, int amountStudents, String startDate, String endDate, boolean archived, boolean validated, String extraInfo, String assignerUserId) {
+        this.title = title;
+        this.type = type;
+        this.task = task;
+        this.amountHours = amountHours;
+        this.amountStudents = amountStudents;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.archived = archived;
+        this.validated = validated;
+        this.extraInfo = extraInfo;
+        this.assignerUserId = assignerUserId;
     }
 
-    public int getFicheID() {
-        return ficheID;
+//    public Assignment(int assignmentId, String title, String type, String task, int amountHours, int amountStudents, String startDate, String endDate, boolean archived, boolean validated, String extraInfo, String assignerUserId) {
+//        this.assignmentId = assignmentId;
+//        this.title = title;
+//        this.type = type;
+//        this.task = task;
+//        this.amountHours = amountHours;
+//        this.amountStudents = amountStudents;
+//        this.startDate = startDate;
+//        this.endDate = endDate;
+//        this.archived = archived;
+//        this.validated = validated;
+//        this.extraInfo = extraInfo;
+//        this.assignerUserId = assignerUserId;
+//    }
+//
+//    public Assignment(int ficheID, String titel, String soort, String omschrijving, int aantalUren, int aantalStudenten, String beginDatum, String eindDatum, String opdrachtgever) {
+//        this.assignmentId = ficheID;
+//        this.title = titel;
+//        this.type = soort;
+//        this.task = omschrijving;
+//        this.amountHours = aantalUren;
+//        this.amountStudents = aantalStudenten;
+//        this.startDate = beginDatum;
+//        this.endDate = eindDatum;
+//        this.assignerUserId = opdrachtgever;
+//    }
+
+    public Integer getAssignmentId() {
+        return assignmentId;
     }
 
-    public String getTitel() {
-        return titel;
+    public String getTitle() {
+        return title;
     }
 
-    public String getSoort() {
-        return soort;
+    public String getType() {
+        return type;
     }
 
-    public String getOmschrijving() {
-        return omschrijving;
+    public String getTask() {
+        return task;
     }
 
-    public int getAantalUren() {
-        return aantalUren;
+    public int getAmountHours() {
+        return amountHours;
     }
 
-    public int getAantalStudenten() {
-        return aantalStudenten;
+    public int getAmountStudents() {
+        return amountStudents;
     }
 
-    public String getBeginDatum() {
-        return beginDatum;
+    public String getStartDate() {
+        return startDate;
     }
 
-    public String getEindDatum() {
-        return eindDatum;
+    public String getEndDate() {
+        return endDate;
     }
 
-    public String getOpdrachtgever() {
-        return opdrachtgever;
+    public String getAssignerUserId() {
+        return assignerUserId;
+    }
+
+    public boolean isArchived() {
+        return archived;
+    }
+
+    public boolean isValidated() {
+        return validated;
+    }
+
+    public String getExtraInfo() {
+        return extraInfo;
     }
 }
--- demo/src/main/java/com/architec/demo/assignment/AssignmentController.java
+++ demo/src/main/java/com/architec/demo/assignment/AssignmentController.java
@@ -4,10 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class AssignmentController {
@@ -16,23 +15,55 @@ public class AssignmentController {
     public AssignmentController() {
         assignments = new ArrayList<>();
 
-        assignments.add(new Assignment(1, "opdracht 1", "soort 1", "omschriving opdracht 1",
-                5, 2, "01/01/2019", "02/02/2019", "opdrachtgever"));
-
-        assignments.add(new Assignment(2, "opdracht 2", "soort 2", "omschriving opdracht 2",
-                5, 2, "01/01/2019", "02/02/2019", "opdrachtgever"));
-
-        assignments.add(new Assignment(3, "opdracht 3", "soort 3", "omschriving opdracht 3",
-                5, 2, "01/01/2019", "02/02/2019", "opdrachtgever"));
+//        assignments.add(new Assignment(1, "opdracht 1", "soort 1", "omschriving opdracht 1",
+//                5, 2, "01/01/2019", "02/02/2019",false, true, "extra info1", "opdrachtgever"));
+//
+//        assignments.add(new Assignment(2, "opdracht 2", "soort 2", "omschriving opdracht 2",
+//                5, 2, "01/01/2019", "02/02/2019", "opdrachtgever"));
+//
+//        assignments.add(new Assignment(3, "opdracht 3", "soort 3", "omschriving opdracht 3",
+//                5, 2, "01/01/2019", "02/02/2019", "opdrachtgever"));
     }
 
     @RequestMapping(value = "/assignments", method = RequestMethod.GET)
-    public String assignments(Model model) {
+    public String assignments(Model model) throws SQLException {
 
-        String test = assignments.get(0).getTitel();
-
-        model.addAttribute("titel", test);
-        model.addAttribute("all", assignments);
+//        String test = assignments.get(0).getTitle();
+//
+//        model.addAttribute("titel", test);
+//        model.addAttribute("all", assignments);
+        Connection c = getConnectionToDb();
+        String result = getAssignment(c);
+        model.addAttribute("assign", result);
+        System.out.println(result);
         return "assignments";
+    }
+
+    public String getAssignment(Connection con) throws SQLException {
+        String result = "";
+        String query = "select * from assignment";
+
+        PreparedStatement prm = con.prepareStatement(query);
+        ResultSet set = prm.executeQuery();
+
+        while (set.next()) {
+            result = result + set.getString("title") + " ";
+            result = result + set.getString("type") + " ";
+        }
+
+        return result;
+    }
+
+    public Connection getConnectionToDb() {
+        try {
+            Connection c = DriverManager.getConnection("jdbc:sqlserver://192.168.84.92;databaseName=crediti;username=sa;password=Zxcvb0123");
+            System.out.println("connected");
+
+            return c;
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            return null;
+        }
     }
 }
