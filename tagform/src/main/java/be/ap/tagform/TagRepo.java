54a9f5ca86312cf03bc2f06cfa891e1297e92ea4
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-04-26 11:51:25 +0200

    deleting unused folders

--- tagform/src/main/java/be/ap/tagform/TagRepo.java
+++
@@ -1,8 +0,0 @@
-package be.ap.tagform;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TagRepo extends JpaRepository<Tag, Integer> {
-}
