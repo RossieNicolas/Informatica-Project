606eb3fcb8e9940e839ff01f32e6b0fb04f496b3
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-04-26 10:26:53 +0200

    fixed controllers

--- demo/src/main/java/com/architec/demo/HelloController.java
+++ demo/src/main/java/com/architec/demo/HelloController.java
@@ -1,15 +1,7 @@
 package com.architec.demo;
-
-import com.architec.demo.models.Tag;
-import com.architec.demo.repositories.TagRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.sql.Connection;
-import java.sql.DriverManager;
 
 @RestController
 public class HelloController {
@@ -17,30 +9,5 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
-    }
-    @Autowired
-    TagRepo tagRepo;
-    @RequestMapping("/tag")
-    public String assignment() {
-        getConnectionToDb();
-        return "tag";
-    }
-
-    @PostMapping("/tag")
-    public Tag createTag(@Valid Tag tag){
-        return tagRepo.save(tag);
-    }
-
-    public Connection getConnectionToDb() {
-        try {
-            Connection c = DriverManager.getConnection("jdbc:sqlserver://192.168.84.92;databaseName=crediti;username=sa;password=Zxcvb0123");
-            System.out.println("connected");
-
-            return c;
-
-        } catch (Exception ex) {
-            System.out.println(ex);
-            return null;
-        }
     }
 }
