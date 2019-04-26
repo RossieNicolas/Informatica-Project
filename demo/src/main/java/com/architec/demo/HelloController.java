31752f83e6b3db5ba294722184eb34d5cfcc51a7
VeraaK <karenveraa@live.be>
2019-04-26 10:26:01 +0200

    G2-9 prepairing merge

--- demo/src/main/java/com/architec/demo/HelloController.java
+++ demo/src/main/java/com/architec/demo/HelloController.java
@@ -1,49 +1,14 @@
 package com.architec.demo;
 
-import java.util.List;
-
-import com.architec.demo.repositories.AssignmentRepository;
-import com.architec.demo.repositories.UserRepository;
-import com.architec.demo.models.Assignment;
-import com.architec.demo.models.User;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
 
-    @Autowired
-    private AssignmentRepository AssignmentRepo;
-
-    @Autowired
-    private UserRepository userRepo;
-
-    private List<Assignment> fiches;
-    private List<User> users;
-
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
-    }
-
-    @GetMapping("/error")
-    public String error() {
-        return "error";
-    }
-
-    @GetMapping("/allassignments")
-    public String getAllAssingments(Model model) {
-        fiches = AssignmentRepo.findAll();
-        users = userRepo.findAll();
-
-        model.addAttribute("assignments", fiches);
-        model.addAttribute("user", users);
-
-        return "listAllAssignments";
     }
 
 }
---
+++ demo/src/main/java/com/architec/demo/controllers/AssignmentController.java
@@ -0,0 +1,42 @@
+package com.architec.demo.controllers;
+
+import java.util.List;
+
+import com.architec.demo.repositories.AssignmentRepository;
+import com.architec.demo.repositories.UserRepository;
+import com.architec.demo.models.Assignment;
+import com.architec.demo.models.User;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AssignmentController {
+
+    @Autowired
+    private AssignmentRepository AssignmentRepo;
+
+    @Autowired
+    private UserRepository userRepo;
+
+    private List<Assignment> fiches;
+    private List<User> users;
+
+    @GetMapping("/error")
+    public String error() {
+        return "error";
+    }
+
+    @GetMapping("/allassignments")
+    public String getAllAssingments(Model model) {
+        fiches = AssignmentRepo.findAll();
+        users = userRepo.findAll();
+
+        model.addAttribute("assignments", fiches);
+        model.addAttribute("user", users);
+
+        return "listAllAssignments";
+    }
+}
