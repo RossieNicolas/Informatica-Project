fd7436e1b1174bd8b8a6f594b2e1da2e286dac63
wa1id <wa1id@users.noreply.github.com>
2019-04-27 17:06:54 +0200

    remove unused db connection method

--- demo/src/main/java/com/architec/demo/controllers/TagController.java
+++ demo/src/main/java/com/architec/demo/controllers/TagController.java
@@ -17,25 +17,11 @@ public class TagController {
     TagRepo tagRepo;
     @RequestMapping("/tag")
     public String assignment() {
-        getConnectionToDb();
         return "tag";
     }
 
     @PostMapping("/tag")
     public Tag createTag(@Valid Tag tag){
         return tagRepo.save(tag);
-    }
-
-    public Connection getConnectionToDb() {
-        try {
-            Connection c = DriverManager.getConnection("jdbc:sqlserver://192.168.84.92;databaseName=crediti;username=sa;password=Zxcvb0123");
-            System.out.println("connected");
-
-            return c;
-
-        } catch (Exception ex) {
-            System.out.println(ex);
-            return null;
-        }
     }
 }
