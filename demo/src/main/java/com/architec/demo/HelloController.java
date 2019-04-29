968f9416fbe5d9ecef37d821305eded6e25d8070
VeraaK <karenveraa@live.be>
2019-04-29 13:47:33 +0200

    G2-4 Merge

--- demo/src/main/java/com/architec/demo/HelloController.java
+++ demo/src/main/java/com/architec/demo/HelloController.java
@@ -1,6 +1,7 @@
 package com.architec.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class HelloController {
