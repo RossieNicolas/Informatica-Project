e8113067e73b4c7792fe0c4eccfcc3c3e1080184
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-05-02 14:59:39 +0200

    G2_10 added input control

--- demo/src/main/java/com/architec/demo/controllers/TagController.java
+++ demo/src/main/java/com/architec/demo/controllers/TagController.java
@@ -2,11 +2,14 @@ package com.architec.demo.controllers;
 
 import com.architec.demo.models.Tag;
 import com.architec.demo.repositories.TagRepo;
+import org.aspectj.bridge.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.security.auth.message.MessageInfo;
 import javax.validation.Valid;
 
 @Controller
@@ -20,7 +23,28 @@ public class TagController {
     }
 
     @PostMapping("/tag")
-    public Tag createTag(@Valid Tag tag) {
-        return tagRepo.save(tag);
+    public void createTag(@Valid Tag tag, Model model) {
+        boolean added = false;
+        for(Tag t : tagRepo.findAll()){
+            if(tag.getTagName().equalsIgnoreCase("")){
+                added = false;
+                model.addAttribute("status", "failed");
+            }
+            else if (tag.getTagName().equalsIgnoreCase(t.getTagName())) {
+                tagRepo.delete(t);
+                tagRepo.save(tag);
+                added = false;
+                model.addAttribute("status", "edit");
+            }
+            else {
+                tagRepo.save(tag);
+                added = true;
+                model.addAttribute("status", "added");
+            }
+        }
+        if (!added){
+
+        }
+
     }
 }
