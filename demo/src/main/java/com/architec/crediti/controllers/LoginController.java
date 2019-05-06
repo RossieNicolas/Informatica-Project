7cc4fdd90e80f50c3999b262eff381195beb6dba
wa1id <wa1id@users.noreply.github.com>
2019-05-06 10:39:21 +0200

    G2-1 Can log in with LDAP credentials

--- demo/src/main/java/com/architec/crediti/controllers/LoginController.java
+++ demo/src/main/java/com/architec/crediti/controllers/LoginController.java
@@ -1,12 +1,12 @@
 package com.architec.crediti.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login")
+    @RequestMapping("/login")
     public String getLogin() {
         return "login";
     }
