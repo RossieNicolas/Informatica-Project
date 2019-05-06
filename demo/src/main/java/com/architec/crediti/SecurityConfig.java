b7d3fd6040010ed49106feca7969d621442b6d6a
wa1id <wa1id@users.noreply.github.com>
2019-05-06 10:15:08 +0200

    G2-1 Integrate Spring Security and show custom login page

---
+++ demo/src/main/java/com/architec/crediti/SecurityConfig.java
@@ -0,0 +1,66 @@
+package com.architec.crediti;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Value("${spring.ldap.urls}")
+    private String ldapUrls;
+    @Value("${spring.ldap.base}")
+    private String ldapBaseDn;
+    @Value("${spring.ldap.username}")
+    private String ldapSecurityPrincipal;
+    @Value("${spring.ldap.password}")
+    private String ldapPrincipalPassword;
+    @Value("${ldap.user.dn.pattern}")
+    private String ldapUserDnPattern;
+    @Value("${ldap.enabled}")
+    private String ldapEnabled;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/login**").permitAll()
+                .antMatchers("/profile/**").fullyAuthenticated()
+                .antMatchers("/").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .failureUrl("/login?error")
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        if (Boolean.parseBoolean(ldapEnabled)) {
+            auth
+                    .ldapAuthentication()
+                    .contextSource()
+                    .url(ldapUrls + ldapBaseDn)
+                    .managerDn(ldapSecurityPrincipal)
+                    .managerPassword(ldapPrincipalPassword)
+                    .and()
+                    .userDnPatterns(ldapUserDnPattern)
+                    .userSearchFilter("userPrincipalName={0}")
+                    .userSearchBase("OU=Studenten,OU=Users,OU=DWAP")
+                    .groupSearchBase("OU=Distribution groups,OU=Groups,OU=DWAP")
+                    .groupRoleAttribute("CN");
+        } else {
+            auth
+                    .inMemoryAuthentication()
+                    .withUser("user").password("password").roles("USER")
+                    .and()
+                    .withUser("admin").password("admin").roles("ADMIN");
+        }
+    }
+}
