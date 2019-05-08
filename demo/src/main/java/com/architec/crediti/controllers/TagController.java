5ec4eb27a292dea2dceddc77f839ac28390dd603
alina.storme@student.ap.be <43607399+alintjax@users.noreply.github.com>
2019-05-08 13:03:42 +0200

    G2-129 and G2-133 HTML changed. Added pages.

--- demo/src/main/java/com/architec/crediti/controllers/TagController.java
+++ demo/src/main/java/com/architec/crediti/controllers/TagController.java
@@ -1,14 +1,21 @@
 package com.architec.crediti.controllers;
 
+import com.architec.crediti.models.Assignment;
+import com.architec.crediti.models.Pager;
 import com.architec.crediti.models.Tag;
+import com.architec.crediti.repositories.Methods;
 import com.architec.crediti.repositories.TagRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class TagController {
@@ -47,11 +54,34 @@ public class TagController {
 
     //list all tags
     @RequestMapping(value = "/listAllTags", method = RequestMethod.GET)
-    public String listAllTags(Model model) /* throws SQLException*/ {
-
+    public ModelAndView listAllTags(@RequestParam("page") Optional<Integer> page) /* throws SQLException*/ {
+        ModelAndView modelAndView = new ModelAndView("listAllTags");
         List<Tag> listAllTags = tagRepo.findAll();
-        model.addAttribute("listAllTags", listAllTags);
-        return "listAllTags";
+
+        //model.addAttribute("listAllTags", listAllTags);
+
+        int initialPage = 0;
+        int pageSize = 15;
+
+        int buttons = (int) tagRepo.count() / pageSize;
+
+        if (tagRepo.count() % pageSize != 0) {
+            buttons++;
+        }
+
+        // Evaluate page. If requested parameter is null or less than 0 (to
+        // prevent exception), return initial size. Otherwise, return value of
+        // param. decreased by 1.
+        int evalPage = (page.orElse(0) < 1) ? initialPage : page.get() - 1;
+
+        Page<Tag> fiches = tagRepo.findAll(PageRequest.of(evalPage, pageSize));
+        Pager pager = new Pager(fiches.getTotalPages(), fiches.getNumber(), buttons);
+
+        modelAndView.addObject("tags", fiches);
+        modelAndView.addObject("listAllTags", fiches);
+        modelAndView.addObject("selectedPageSize", pageSize);
+        modelAndView.addObject("pager", pager);
+        return modelAndView;
     }
 
     //list specific tag
