36766bf06c229354e149d50e91eddce6df31d912
VeraaK <karenveraa@live.be>
2019-05-08 14:14:00 +0200

    unused imports

--- demo/src/main/java/com/architec/crediti/repositories/TagRepo.java
+++ demo/src/main/java/com/architec/crediti/repositories/TagRepo.java
@@ -4,8 +4,6 @@ import com.architec.crediti.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface TagRepo extends JpaRepository<Tag, Integer> {
     Tag findBytagId(int tag_id);
