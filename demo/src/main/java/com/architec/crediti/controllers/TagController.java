b7d393fa8a6336d993df5b0482a33f61666eb682
VeraaK <karenveraa@live.be>
2019-05-09 13:51:37 +0200

    G2-11

--- demo/src/main/java/com/architec/crediti/controllers/TagController.java
+++ demo/src/main/java/com/architec/crediti/controllers/TagController.java
@@ -27,7 +27,7 @@ public class TagController {
     @Autowired
     TagRepo tagRepo;
 
-    //get create tag page
+    // get create tag page
     @RequestMapping("/tag")
     public String assignment() {
         return "tag";
@@ -39,28 +39,28 @@ public class TagController {
         return "tag";
     }
 
-    //make a new tag
+    // make a new tag
     @PostMapping("/tag")
-    public String createTag(Model model, @RequestParam ("tagName") String tagName, @RequestParam ("tagDescription") String tagDescription) {
+    public String createTag(Model model, @RequestParam("tagName") String tagName,
+            @RequestParam("tagDescription") String tagDescription) {
         Tag tag = new Tag(tagName, tagDescription);
         boolean existsName = tagRepo.existsByTagName(tagName);
-        if (!existsName){
+        if (!existsName) {
             tagRepo.save(tag);
-        }
-        else    {
+        } else {
             model.addAttribute("error", "Tag naam bestaat al!");
             return "tag";
         }
         return "redirect:/listAllTags";
     }
 
-    //list all tags
+    // list all tags
     @RequestMapping(value = "/listAllTags", method = RequestMethod.GET)
-    public ModelAndView listAllTags(@RequestParam("page") Optional<Integer> page) /* throws SQLException*/ {
+    public ModelAndView listAllTags(@RequestParam("page") Optional<Integer> page) /* throws SQLException */ {
         ModelAndView modelAndView = new ModelAndView("listAllTags");
         List<Tag> listAllTags = tagRepo.findAll();
 
-        //model.addAttribute("listAllTags", listAllTags);
+        // model.addAttribute("listAllTags", listAllTags);
 
         int initialPage = 0;
         int pageSize = 15;
@@ -86,7 +86,7 @@ public class TagController {
         return modelAndView;
     }
 
-    //list specific tag
+    // list specific tag
     @RequestMapping(value = "/listAllTags/{tag_id}", method = RequestMethod.GET)
     public String tags(@PathVariable("tag_id") int tag_id, Model model) {
 
@@ -96,25 +96,27 @@ public class TagController {
         return "editTag";
     }
 
-    //update specific tag
+    // update specific tag
     @PostMapping(value = "/listAllTags/{id}")
-    public String saveTag(Model model, @PathVariable("id") int id,@Valid Tag tag, @RequestParam ("tagName") String tagName,  @RequestParam ("tagDescription") String tagDescription) {
-        tag = tagRepo.findBytagId(id);
+    public String saveTag(Model model, @PathVariable("id") int id, @Valid Tag tag,
+            @RequestParam("tagName") String tagName, @RequestParam("tagDescription") String tagDescription) {
+        Tag tagForId = tagRepo.findBytagId(id);
 
         boolean existsName = tagRepo.existsByTagName(tagName);
-        if (!existsName){
+        if (!existsName) {
             tag.setTagName(tagName);
             tag.setTagDescription(tagDescription);
             tagRepo.save(tag);
-        } else    {
+        } else {
             model.addAttribute("error", "Tag naam bestaat al!");
+            model.addAttribute("tags", tagForId);
             return "editTag";
-            //TODO return form with existing values of specific id
+            // TODO return form with existing values of specific id
         }
         return "redirect:/listAllTags";
     }
 
-    //delete specific tag
+    // delete specific tag
     @GetMapping("/deletetag/{id}")
     public String deleteTag(@PathVariable("id") int id, Model model) {
         Tag tag = tagRepo.findById(id).orElseThrow(() -> new IllegalArgumentException("Invalid tag Id:" + id));
@@ -122,8 +124,5 @@ public class TagController {
         model.addAttribute("tags", tagRepo.findAll());
         return "redirect:/listAllTags";
     }
-
-
-
 
 }
