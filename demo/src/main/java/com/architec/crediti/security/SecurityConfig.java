e60ae2da7b9abe8ca9c092fd1bf5c207868125f7
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-05-09 15:40:01 +0200

    fixed date problem and redirects before login

--- demo/src/main/java/com/architec/crediti/security/SecurityConfig.java
+++ demo/src/main/java/com/architec/crediti/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 //pagina's die niet-ingelogde gebruikers zien
                 .antMatchers(staticResources).permitAll()
-                .antMatchers("/login", "/").permitAll()
+                .antMatchers("/login", "/","/createexternaluser","createexternal","registersucces").permitAll()
                 //Alle andere pagina's blokkeren
                 .anyRequest().fullyAuthenticated()
                 .and()
