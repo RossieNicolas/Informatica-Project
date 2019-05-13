70833f08c55ce6d8fbf413e103077f1a25f9ccb1
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-05-13 14:53:47 +0200

    G2_23 first version

--- demo/src/main/java/com/architec/crediti/controllers/ArchiveController.java
+++ demo/src/main/java/com/architec/crediti/controllers/ArchiveController.java
@@ -1,14 +1,15 @@
 package com.architec.crediti.controllers;
 
 import com.architec.crediti.models.ArchivedAssignment;
-import com.architec.crediti.models.Assignment;
 import com.architec.crediti.models.Pager;
 import com.architec.crediti.repositories.ArchiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,11 +19,14 @@ import java.util.Optional;
 public class ArchiveController {
     @Autowired
     ArchiveRepository archiveRepo;
+    int initialPage = 0;
+    int pageSize = 15;
+
+    //get archive page
     @GetMapping("/archive")
     public ModelAndView showPersonsPage(@RequestParam("page") Optional<Integer> page) {
         ModelAndView modelAndView = new ModelAndView("archive");
-        int initialPage = 0;
-        int pageSize = 15;
+
 
         int buttons = (int) archiveRepo.count() / pageSize;
 
@@ -43,5 +47,41 @@ public class ArchiveController {
         modelAndView.addObject("selectedPageSize", pageSize);
         modelAndView.addObject("pager", pager);
         return modelAndView;
+    }
+
+    //search in archive
+    @PostMapping("/archive")
+    String getArchivedAssignment(@RequestParam("searchbar") String name, Model model, @RequestParam("page") Optional<Integer> page) {
+
+        try {
+            ArchivedAssignment a = archiveRepo.findByAssignmentId((Integer.parseInt(name)));
+            model.addAttribute("assignments", a);
+
+        } catch (Exception e) {
+            model.addAttribute("assignments", archiveRepo.findByTitleContaining(name));
+        }
+
+        ModelAndView modelAndView = new ModelAndView("Archive");
+
+
+        int buttons = (int) archiveRepo.count() / pageSize;
+
+        if (archiveRepo.count() % pageSize != 0) {
+            buttons++;
+        }
+
+        // Evaluate page. If requested parameter is null or less than 0 (to
+        // prevent exception), return initial size. Otherwise, return value of
+        // param. decreased by 1.
+        int evalPage = (page.orElse(0) < 1) ? initialPage : page.get() - 1;
+
+        Page<ArchivedAssignment> fiches = archiveRepo.findAll(PageRequest.of(evalPage, pageSize));
+        Pager pager = new Pager(fiches.getTotalPages(), fiches.getNumber(), buttons);
+
+        model.addAttribute("persons", fiches);
+        model.addAttribute("selectedPageSize", pageSize);
+        model.addAttribute("pager", pager);
+
+        return "archive";
     }
 }
--- demo/src/main/java/com/architec/crediti/repositories/ArchiveRepository.java
+++ demo/src/main/java/com/architec/crediti/repositories/ArchiveRepository.java
@@ -1,8 +1,11 @@
 package com.architec.crediti.repositories;
 
 import com.architec.crediti.models.ArchivedAssignment;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.util.List;
+
 public interface ArchiveRepository extends PagingAndSortingRepository<ArchivedAssignment, Long> {
+    ArchivedAssignment findByAssignmentId(long assignmentId);
+    List<ArchivedAssignment> findByTitleContaining(String name);
 }
