bdcca53d6ef8c741fddbb06dca31ec70258bc1db
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-05-17 13:11:30 +0200

    refactor password recovery tests

---
+++ demo/src/test/com/architec/crediti/PasswordRecoveryTest.java
@@ -0,0 +1,65 @@
+package com.architec.crediti;
+
+
+import com.architec.crediti.models.User;
+import com.architec.crediti.repositories.UserRepository;
+import com.architec.crediti.security.Role;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PasswordRecoveryTest {
+
+    @Autowired
+    private UserRepository users;
+    @Test
+    public void FindByEmailShouldReturnCorrectUser() {
+
+        //arrange
+        User u = new User("testUser", "testUser","test@test.test", Role.COORDINATOR,false);
+        users.save(u);
+        boolean exists = false;
+
+        //act
+        for(User copyU : users.findAll()){
+            if (copyU.getEmail().equalsIgnoreCase(u.getEmail())){
+                exists = true;
+            }
+        }
+
+        //assert
+        assertTrue(exists);
+
+        //undo operation
+        users.delete(u);
+    }
+
+    @Test
+    public void NotExistingEmailShouldReturnFalse() {
+
+        //arrange
+        List<User> list = users.findAll();
+        User u = users.findByEmail("notAnActualEmail");
+        //TODO
+        boolean exists = false;
+
+        //act
+        if (u == null){
+            exists = false;
+        }
+
+        //assert
+        assertFalse(exists);
+
+    }
+
+}
