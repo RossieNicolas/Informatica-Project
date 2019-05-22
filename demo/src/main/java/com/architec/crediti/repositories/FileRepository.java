9abb5f540ad25bbc237096dc3ebbf7bb038f886b
VeraaK <karenveraa@live.be>
2019-05-22 11:41:18 +0200

    G2-15

--- demo/src/main/java/com/architec/crediti/repositories/FileRepository.java
+++ demo/src/main/java/com/architec/crediti/repositories/FileRepository.java
@@ -6,11 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface FileRepository extends JpaRepository<File, Long> {
 
     List<File> findByUser(User user);
 
-    File findByTitle(String name);
+    Optional<File> findByFileId(int id);
 }
