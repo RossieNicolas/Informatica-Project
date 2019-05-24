dcd5992e5f51b425b5bbc0ebd368716ed8935b44
VeraaK <karenveraa@live.be>
2019-05-24 09:37:12 +0200

    G2 vps

--- demo/src/main/java/com/architec/crediti/controllers/LoginController.java
+++ demo/src/main/java/com/architec/crediti/controllers/LoginController.java
@@ -11,13 +11,13 @@ public class LoginController {
 
     @GetMapping("/login")
     public String getLogin() {
-        return "/basic/login";
+        return "/login";
     }
 
     @GetMapping("/loginError")
     public String loginError(Model model) {
         model.addAttribute("loginError", true);
-        return "/basic/login";
+        return "/login";
     }
 
     @GetMapping("/logout")
