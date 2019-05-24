1181a53ae6642c2cbbc41135b6e5e18056b85d8e
RossieNicolas <43535740+RossieNicolas@users.noreply.github.com>
2019-05-24 12:36:56 +0200

    correct order for assignments, fix search assignments

--- demo/src/main/java/com/architec/crediti/repositories/AssignmentRepository.java
+++ demo/src/main/java/com/architec/crediti/repositories/AssignmentRepository.java
@@ -6,7 +6,9 @@ import com.architec.crediti.models.User;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,7 +18,12 @@ public interface AssignmentRepository extends PagingAndSortingRepository<Assignm
     Assignment findByAssignmentId(long assignmentId);
     List<Assignment> findByTitleContainingAndArchived(String title, boolean archived);
     List<Assignment> findByAssignerUserId(User user);
+    Page<Assignment> findByTitleContainingAndFullOrderByAssignmentIdDesc(String title, boolean full,Pageable pageable);
     Page<Assignment> findByTitleContainingAndArchived(String title, boolean archived,Pageable pageable);
-    Page<Assignment> findByFull(boolean full, Pageable pageable);
     Page<Assignment> findByFullOrderByAssignmentIdDesc(boolean full, Pageable pageable);
+    List<Assignment> findByTitleContainingAndFullOrderByAssignmentIdDesc(String title, boolean full);
+
+    @Query(value ="select * from assignments a where a.assignment_id in :tagId",nativeQuery=true)
+    Page<Assignment> findByTagsOrderByAssignmentIdDesc(@Param("tagId") List<Long> tagId, Pageable pageable );
+
 }
