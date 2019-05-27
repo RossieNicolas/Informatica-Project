f8d62abb10976705029ec00f69964a73d956a1c4
wa1id <wa1id@users.noreply.github.com>
2019-05-27 22:56:23 +0200

    Add missing '@Repository' annotations

--- demo/src/main/java/com/architec/crediti/repositories/ArchiveRepository.java
+++ demo/src/main/java/com/architec/crediti/repositories/ArchiveRepository.java
@@ -2,9 +2,11 @@ package com.architec.crediti.repositories;
 
 import com.architec.crediti.models.ArchivedAssignment;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ArchiveRepository extends PagingAndSortingRepository<ArchivedAssignment, Long> {
     ArchivedAssignment findByAssignmentId(long assignmentId);
     List<ArchivedAssignment> findByTitleContaining(String name);
--- demo/src/main/java/com/architec/crediti/repositories/DocumentationRepository.java
+++ demo/src/main/java/com/architec/crediti/repositories/DocumentationRepository.java
@@ -2,9 +2,11 @@ package com.architec.crediti.repositories;
 
 import com.architec.crediti.models.Documentation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface DocumentationRepository extends JpaRepository<Documentation, Long> {
     Documentation findByTitle(String name);
 
