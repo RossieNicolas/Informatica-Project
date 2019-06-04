8dda79a9ff343c6cc0736761fd6281093e9a7057
VeraaK <karenveraa@live.be>
2019-06-04 11:25:39 +0200

    G2-340 delete coordinator

--- demo/src/main/java/com/architec/crediti/controllers/CoordinatorController.java
+++ demo/src/main/java/com/architec/crediti/controllers/CoordinatorController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -47,6 +48,25 @@ public class CoordinatorController {
         User usr = userRepo.findByEmail(email);
         usr.setRole(Role.COORDINATOR);
         userRepo.save(usr);
+        return "redirect:/coordinator";
+    }
+
+    @GetMapping("/deletecoordinator/{id}")
+    public String deleteCoordiantor(@PathVariable("id") int id){
+        User usr = userRepo.findByUserId((long) id);
+
+        switch (usr.getEmail().substring(0,1).toLowerCase()){
+            case "s":
+                usr.setRole(Role.STUDENT);
+                break;
+            case "p":
+                usr.setRole(Role.DOCENT);
+            default:
+                usr.setRole(Role.EXTERN);
+        }
+
+        userRepo.save(usr);
+
         return "redirect:/coordinator";
     }
 }
