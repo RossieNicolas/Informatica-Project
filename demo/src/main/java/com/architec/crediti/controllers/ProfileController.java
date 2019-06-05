b5cea0760758f10f63b7e569a74c41de36d12dab
VeraaK <karenveraa@live.be>
2019-06-05 11:00:46 +0200

    G2-29 process portfolio G2-227 hours student

--- demo/src/main/java/com/architec/crediti/controllers/ProfileController.java
+++ demo/src/main/java/com/architec/crediti/controllers/ProfileController.java
@@ -1,8 +1,7 @@
 package com.architec.crediti.controllers;
 
-import com.architec.crediti.models.File;
-import com.architec.crediti.models.Student;
-import com.architec.crediti.models.User;
+import com.architec.crediti.models.*;
+import com.architec.crediti.repositories.AssignmentRepository;
 import com.architec.crediti.repositories.FileRepository;
 import com.architec.crediti.repositories.StudentRepository;
 import com.architec.crediti.repositories.UserRepository;
@@ -12,26 +11,35 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 public class ProfileController {
 
     private final
     StudentRepository stuRepo;
-    private final UserRepository userRepo;
+
+    private final
+    UserRepository userRepo;
+
+    private final
+    AssignmentRepository assignmentRepo;
 
 
     private final
     FileRepository fileRepo;
 
     @Autowired
-    public ProfileController(StudentRepository stuRepo , FileRepository fileRepo, UserRepository userRepo) {
+    public ProfileController(StudentRepository stuRepo , FileRepository fileRepo, UserRepository userRepo, AssignmentRepository assRepo) {
         this.stuRepo = stuRepo;
         this.fileRepo = fileRepo;
         this.userRepo = userRepo;
+        this.assignmentRepo = assRepo;
     }
 
     @GetMapping("liststudents/{studentnumber}")
@@ -48,6 +56,53 @@ public class ProfileController {
         User currentUser = userRepo.findByEmail(principal.getName());
         model.addAttribute("name",currentUser.getFirstname() + " " + currentUser.getLastname().substring(0,1) + ".");
         return "student/studentDetail";
+    }
+
+    @GetMapping("/changestatus/{studentId}/{assignmentId}")
+    public String changeStatus(@PathVariable("studentId") String userId, @PathVariable("assignmentId") int assignmentId){
+        int studentId = Integer.parseInt(userId);
+        Student usr = stuRepo.findByStudentNumber(userId);
+        User u = userRepo.findByUserId(usr.getUserId().getUserId());
+
+        List<File> files = fileRepo.findByUser(u);
+        List<File> rightFiles = new ArrayList<>();
+        File file = null;
+
+        for(File f : files) {
+            if(f.getAssignmentId() == assignmentId){
+                rightFiles.add(f);
+
+                if(f.getDocType().equalsIgnoreCase("Contract")) {
+                    file = f;
+                }
+            }
+        }
+
+        String status = file.getStatus();
+        String newStatus = "start";
+        if(status.equals(Progress.START.getInfo())){
+            newStatus = Progress.CONTRACT.getInfo();
+        } else if(status.equals(Progress.CONTRACT.getInfo())){
+            newStatus = Progress.PORTFOLIO.getInfo();
+        } else if(status.equals(Progress.PORTFOLIO.getInfo())){
+            newStatus = Progress.BEWIJS.getInfo();
+        }else if(status.equals(Progress.BEWIJS.getInfo())){
+            newStatus = Progress.END.getInfo();
+            Assignment ass = assignmentRepo.findByAssignmentId(file.getAssignmentId());
+            usr.setAmoutHours(usr.getAmoutHours() + ass.getAmountHours());
+        } else {
+            newStatus = Progress.END.getInfo();
+        }
+
+        for(File f : rightFiles) {
+            f.setStatus(newStatus);
+        }
+
+        fileRepo.save(file);
+        stuRepo.save(usr);
+        userRepo.save(u);
+
+        return "redirect:/liststudents/"+userId;
     }
 
 }
